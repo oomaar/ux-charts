413eecd1aec8a27d6b9a18c7073b09a3e82e7214
Omar <omaar_5@hotmail.com>
2022-04-06 22:13:12 +0200

    fix the data fetch error

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -24,68 +24,66 @@ import { SearchBar } from "components/Navbars/SearchBar/SearchBar";
 import dashboardData from "../../data/dashboardData.json";
 
 export default function Dashboard() {
-  const [appPerformance, setAppPerformance] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [deviceSearchTerm, setDeviceSearchTerm] = useState("");
   const devicesTable = dashboardData.devicesTable;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(
-        `https://my-json-server.typicode.com/WorexEG/json-server/appPerformance`
-      ).then(data => data.json());
-      setAppPerformance(res);
-    };
-
-    fetchData();
-  }, []);
+  const appPerformance = dashboardData.chartsData;
+  console.log(
+    "🚀 ~ file: Dashboard.js ~ line 32 ~ Dashboard ~ appPerformance",
+    appPerformance
+  );
 
   return (
-    <Flex flexDirection='column' pt={{ base: "120px", md: "75px" }}>
+    <Flex flexDirection="column" pt={{ base: "120px", md: "75px" }}>
       <Grid
         templateColumns={{ sm: "1fr", md: "1fr", "2xl": "1fr 1fr" }}
-        my='26px'
-        gap='18px'>
+        my="26px"
+        gap="18px"
+      >
         {/* Referral Tracking */}
-        {appPerformance.slice(0, 4).map(app => (
+        {appPerformance.map((app) => (
           <Card gridArea={{ md: "2 / 2 / 3 / 3", "2xl": "auto" }}>
-            <Flex direction='column'>
-              <Flex justify='space-between' align='center' mb='40px'>
-                <Text color='#fff' fontSize='lg' fontWeight='bold'>
+            <Flex direction="column">
+              <Flex justify="space-between" align="center" mb="40px">
+                <Text color="#fff" fontSize="lg" fontWeight="bold">
                   {app.applicationName}
                 </Text>
               </Flex>
               <Flex direction={{ sm: "column", md: "row" }}>
                 <Flex
-                  direction='column'
+                  direction="column"
                   me={{ md: "6px", lg: "52px" }}
-                  mb={{ sm: "16px", md: "0px" }}>
+                  mb={{ sm: "16px", md: "0px" }}
+                >
                   <Flex
-                    direction='column'
-                    p='22px'
+                    direction="column"
+                    p="22px"
                     pe={{ sm: "22e", md: "8px", lg: "22px" }}
                     minW={{ sm: "220px", md: "140px", lg: "220px" }}
-                    bg='linear-gradient(126.97deg, #060C29 28.26%, rgba(4, 12, 48, 0.5) 91.2%)'
-                    borderRadius='20px'
-                    mb='20px'>
-                    <Text color='gray.400' fontSize='sm' mb='4px'>
+                    bg="linear-gradient(126.97deg, #060C29 28.26%, rgba(4, 12, 48, 0.5) 91.2%)"
+                    borderRadius="20px"
+                    mb="20px"
+                  >
+                    <Text color="gray.400" fontSize="sm" mb="4px">
                       Success
                     </Text>
-                    <Text color='#fff' fontSize='lg' fontWeight='bold'>
+                    <Text color="#fff" fontSize="lg" fontWeight="bold">
                       {app.successRate}%
                     </Text>
                   </Flex>
                   <Flex
-                    direction='column'
-                    p='22px'
+                    direction="column"
+                    p="22px"
                     pe={{ sm: "22px", md: "8px", lg: "22px" }}
                     minW={{ sm: "170px", md: "140px", lg: "170px" }}
-                    bg='linear-gradient(126.97deg, #060C29 28.26%, rgba(4, 12, 48, 0.5) 91.2%)'
-                    borderRadius='20px'>
-                    <Text color='gray.400' fontSize='sm' mb='4px'>
+                    bg="linear-gradient(126.97deg, #060C29 28.26%, rgba(4, 12, 48, 0.5) 91.2%)"
+                    borderRadius="20px"
+                  >
+                    <Text color="gray.400" fontSize="sm" mb="4px">
                       Failure
                     </Text>
-                    <Text color='#fff' fontSize='lg' fontWeight='bold'>
+                    <Text color="#fff" fontSize="lg" fontWeight="bold">
                       {app.failerRate}%
                     </Text>
                   </Flex>
@@ -98,8 +96,8 @@ export default function Dashboard() {
                       window.innerWidth >= 1024
                         ? 200
                         : window.innerWidth >= 768
-                          ? 170
-                          : 200
+                        ? 170
+                        : 200
                     }
                     isGradient
                     gradient={{
@@ -107,13 +105,15 @@ export default function Dashboard() {
                       startColor: "rgba(5, 205, 153, 0)",
                       stopColor: "#05CD99",
                     }}
-                    emptyColor='transparent'>
-                    <Flex direction='column' justify='center' align='center'>
+                    emptyColor="transparent"
+                  >
+                    <Flex direction="column" justify="center" align="center">
                       <Text
-                        color='#fff'
+                        color="#fff"
                         fontSize={{ md: "36px", lg: "50px" }}
-                        fontWeight='bold'
-                        mb='4px'>
+                        fontWeight="bold"
+                        mb="4px"
+                      >
                         {app.successRate}%
                       </Text>
                     </Flex>
@@ -124,122 +124,138 @@ export default function Dashboard() {
           </Card>
         ))}
       </Grid>
-      <Grid
-        templateColumns={{ sm: "1fr", md: "1fr", lg: "1fr" }}
-        gap='24px'>
+      <Grid templateColumns={{ sm: "1fr", md: "1fr", lg: "1fr" }} gap="24px">
         {/* Projects */}
-        <Card p='16px' overflowX={{ sm: "scroll", xl: "hidden" }}>
-          <CardHeader p='12px 0px 28px 0px'>
-            <Flex direction='column'>
-              <Text fontSize='lg' color='#fff' fontWeight='bold' pb='8px'>
+        <Card p="16px" overflowX={{ sm: "scroll", xl: "hidden" }}>
+          <CardHeader p="12px 0px 28px 0px">
+            <Flex direction="column">
+              <Text fontSize="lg" color="#fff" fontWeight="bold" pb="8px">
                 All The Applications
               </Text>
-              <Flex align='center'>
-                <Text fontSize='sm' color='gray.400' fontWeight='normal'>
+              <Flex align="center">
+                <Text fontSize="sm" color="gray.400" fontWeight="normal">
                   <SearchBar setSearchTerm={setSearchTerm} />
                 </Text>
               </Flex>
             </Flex>
           </CardHeader>
-          <Table variant='simple' color='#fff'>
+          <Table variant="simple" color="#fff">
             <Thead>
-              <Tr my='.8rem' ps='0px'>
+              <Tr my=".8rem" ps="0px">
                 <Th
-                  color='gray.400'
-                  fontFamily='Plus Jakarta Display'
-                  borderBottomColor='#56577A'>
+                  color="gray.400"
+                  fontFamily="Plus Jakarta Display"
+                  borderBottomColor="#56577A"
+                >
                   Application Name
                 </Th>
                 <Th
-                  color='gray.400'
-                  fontFamily='Plus Jakarta Display'
-                  borderBottomColor='#56577A'>
+                  color="gray.400"
+                  fontFamily="Plus Jakarta Display"
+                  borderBottomColor="#56577A"
+                >
                   Failure Percentage
                 </Th>
               </Tr>
             </Thead>
             <Tbody>
-              {appPerformance.filter(row => {
-                if (searchTerm == "") {
-                  return row
-                } else if (row.applicationName.toLowerCase().includes(searchTerm.toLowerCase())) {
-                  return row
-                }
-              }).map((row) => {
-                return (
-                  <DashboardTableRow
-                    name={row.applicationName}
-                    budget={row.failerRate}
-                  />
-                );
-              })}
+              {appPerformance
+                .filter((row) => {
+                  if (searchTerm == "") {
+                    return row;
+                  } else if (
+                    row.applicationName
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase())
+                  ) {
+                    return row;
+                  }
+                })
+                .map((row) => {
+                  return (
+                    <DashboardTableRow
+                      name={row.applicationName}
+                      budget={row.failerRate}
+                    />
+                  );
+                })}
             </Tbody>
           </Table>
         </Card>
-        <Card p='16px' overflowX={{ sm: "scroll", xl: "hidden" }}>
-          <CardHeader p='12px 0px 28px 0px'>
-            <Flex direction='column'>
-              <Text fontSize='lg' color='#fff' fontWeight='bold' pb='8px'>
+        <Card p="16px" overflowX={{ sm: "scroll", xl: "hidden" }}>
+          <CardHeader p="12px 0px 28px 0px">
+            <Flex direction="column">
+              <Text fontSize="lg" color="#fff" fontWeight="bold" pb="8px">
                 Devices
               </Text>
-              <Flex align='center'>
-                <Text fontSize='sm' color='gray.400' fontWeight='normal'>
+              <Flex align="center">
+                <Text fontSize="sm" color="gray.400" fontWeight="normal">
                   <SearchBar setSearchTerm={setDeviceSearchTerm} />
                 </Text>
               </Flex>
             </Flex>
           </CardHeader>
-          <Table variant='simple' color='#fff'>
+          <Table variant="simple" color="#fff">
             <Thead>
-              <Tr my='.8rem' ps='0px'>
+              <Tr my=".8rem" ps="0px">
                 <Th
-                  ps='0px'
-                  color='gray.400'
-                  fontFamily='Plus Jakarta Display'
-                  borderBottomColor='#56577A'>
+                  ps="0px"
+                  color="gray.400"
+                  fontFamily="Plus Jakarta Display"
+                  borderBottomColor="#56577A"
+                >
                   Application Name
                 </Th>
                 <Th
-                  color='gray.400'
-                  fontFamily='Plus Jakarta Display'
-                  borderBottomColor='#56577A'>
+                  color="gray.400"
+                  fontFamily="Plus Jakarta Display"
+                  borderBottomColor="#56577A"
+                >
                   Username
                 </Th>
                 <Th
-                  color='gray.400'
-                  fontFamily='Plus Jakarta Display'
-                  borderBottomColor='#56577A'>
+                  color="gray.400"
+                  fontFamily="Plus Jakarta Display"
+                  borderBottomColor="#56577A"
+                >
                   Date
                 </Th>
                 <Th
-                  color='gray.400'
-                  fontFamily='Plus Jakarta Display'
-                  borderBottomColor='#56577A'>
+                  color="gray.400"
+                  fontFamily="Plus Jakarta Display"
+                  borderBottomColor="#56577A"
+                >
                   Time
                 </Th>
               </Tr>
             </Thead>
             <Tbody>
-              {devicesTable.filter(row => {
-                if (deviceSearchTerm == "") {
-                  return row
-                } else if (row.applicationName.toLowerCase().includes(deviceSearchTerm.toLowerCase())) {
-                  return row
-                }
-              }).map((row) => {
-                return (
-                  <DashboardTableRow
-                    name={row.applicationName}
-                    userName={row.deviceUserName}
-                    date={row.Date}
-                    time={row.Time}
-                  />
-                );
-              })}
+              {devicesTable
+                .filter((row) => {
+                  if (deviceSearchTerm == "") {
+                    return row;
+                  } else if (
+                    row.applicationName
+                      .toLowerCase()
+                      .includes(deviceSearchTerm.toLowerCase())
+                  ) {
+                    return row;
+                  }
+                })
+                .map((row) => {
+                  return (
+                    <DashboardTableRow
+                      name={row.applicationName}
+                      userName={row.deviceUserName}
+                      date={row.Date}
+                      time={row.Time}
+                    />
+                  );
+                })}
             </Tbody>
           </Table>
         </Card>
       </Grid>
     </Flex>
   );
-};
+}
